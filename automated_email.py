c5103303256198aa6b1bb8c86776c95334dbc591
ashutoshtiwari11 <115335561+ashutoshtiwari11@users.noreply.github.com>
2023-02-09 12:50:43 +0530

    updated version of automated_email.py
    This adds more features to file.

--- automated_email.py
+++ automated_email.py
@@ -1,6 +1,4 @@
-#First create a password in your account e.g. google accounts security section you can create a app password for mail ---- for my computer windows mail section. and keep that password secure.
-
-
+#First create a password in your e.g. google  security section you can create a app password for mail ---- for my computer windows mail section. and keep that password secure.
 
 from email.message import EmailMessage
 import getpass
@@ -10,16 +8,40 @@ import getpass
 
 import smtplib
 
-print("This is the being print just to show that your file has started running.")
-#("pass **************")
+print("This is being print just to show that your file has start running.")
+#("pass ***********")
 
 message=EmailMessage() 
 #The line message = EmailMessage() creates an instance of the EmailMessage class from the email module in the Python Standard Library
 
-print(message)
+
+def add_attachment():
+    import os.path
+    attachment_path=input("Please enter the path of your attachment. \n:")
+    attachment_filename= os.path.basename(attachment_path)
+    import mimetypes
+    mime_type=mimetypes.guess_type(attachment_path)
+    print(mime_type)
+    #separating mime type and sub type it is a tuple.
+    mime_type, mime_subtype=mime_type[0].split('/',1)
+    print(mime_type)
+    print(mime_subtype)
+    
+    
+    with open(attachment_path,'rb') as ap:
+        try:
+         message.add_attachment(ap.read(),
+                               maintype=mime_type, 
+                               subtype=mime_subtype,
+                               filename=attachment_filename)
+        except:
+            print("there was an error in attachment addition.")
+            
+            
+
 print("Ya! it seems to be working fine.")
 sender="ashutoshtiwari6143@gmail.com"
-recipient="***********@gmail.com"
+recipient="neerutiwari12345@gmail.com"
 
 #message['From'] is accessing the 'From' header field of an EmailMessage object in Python. note header field are case insensitive.
 
@@ -27,16 +49,23 @@ message["From"]=sender;
 message["To"]=recipient;
 message["Subject"]="Greeting from {}, please do not reply as this is an automated email".format(recipient)
 
-body=input("Please Enter your content in text form. \n -: ")
-message.set_content(body)
-
 print(message)
+attachment=input("Do You want to add an attachment ? If yes type y else type n. \n:")
+if(attachment == "y"):
+    add_attachment()
+    #I will not print message in case of attachment because it will take long to print in case of image or long pdf.
+else:
+    body=input("Please Enter your content in text form. \n -: ")
+    message.set_content(body)
+    print(message)
+
+
 
 mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
 
 #This SSL/TLS is the same protocol that's used to add a secure transmission layer to HTTP, making it HTTPS. Within the smtplib, there are two classes for making connections to an SMTP server: The SMTP class will make a direct SMTP connection, and the SMTP_SSL class will make a SMTP connection over SSL/TLS. 
 
-mail_server.set_debuglevel(1)
+#mail_server.set_debuglevel(1)
 
 #debuglevel - 1 to show what is happening behind the scene.
 
@@ -48,6 +77,6 @@ mail_server.login(sender,password )
 
 print("login was succesful in case you are seeing this may be...")
 mail_server.send_message(message)
-mail_server.set_debuglevel(1)
+#mail_server.set_debuglevel(1)
 mail_server.quit()
 #so its important to quit the server.
